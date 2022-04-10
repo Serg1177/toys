9e9c000cc9ef7f1fb8f11edc25179ecafd675d6c
Serg1177 <98102615+Serg1177@users.noreply.github.com>
2022-04-10 16:51:55 +0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ let CATALOG = [];
 //https://myjson.dit.upm.es/api/bins/h0s1
 //components/server/catalog.json
 
-fetch('https://myjson.dit.upm.es/api/bins/h0s1')
+fetch('components/server/catalog.json')
 .then(res=>res.json())
 .then(body=>{
 
